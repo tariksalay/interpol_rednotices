8cf593bee662f445bfc393a5b6abb78ea2283dda
Tarik Salay <tariksalay@gmail.com>
2024-04-23 04:11:02 +0300

    head

--- Container_B/webapp.py
+++ Container_B/webapp.py
@@ -1,11 +1,9 @@
 # Publish the queue stored in DB on web
-# Flask
-# streamlit vs flask
 
 import flask
 import psycopg2
 from datetime import datetime
-import pytz
+import pytz # to get the time
 
 # Initialize the Flask app
 webapp = flask.Flask(__name__)
@@ -16,13 +14,12 @@ current_time = datetime.now(tz=turkey_timezone).strftime("%Y-%m-%d %H:%M:%S")
 
 # Establish connection to PostgreSQL
 connection = psycopg2.connect(
-    dbname="mydb",
+    dbname="postgres",
     user="postgres",
-    password="postgres",
+    password="password",
     host="localhost",
     port="5432"
 )
-
 
 # Source: evieplus Academy Python - Flask 161/162
 # https://www.youtube.com/watch?v=wUKb_LC23NM&list=PLtTs2BKyiS4DLNLTVJ1ZtapaCn5wtjI_k&index=3&ab_channel=evieplusAcademy
@@ -34,7 +31,7 @@ def definition():
     # Create a cursor for executing SQL commands
     cursor = connection.cursor()
 
-    # Execute an SQL query to fetch data from the 'interpol_rednotices' table
+    # Query the 'interpol_rednotices' table to pull all
     cursor.execute("SELECT * FROM interpol_rednotices LIMIT 20")
     data = cursor.fetchall()
 
@@ -44,7 +41,6 @@ def definition():
 
     # Render the template from Directory created, and pass the data
     return flask.render_template("definition.html", data=data, current_time=current_time)
-
 
 # Run the Flask app if this script is executed directly
 if __name__ == "__main__":
